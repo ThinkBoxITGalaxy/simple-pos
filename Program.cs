5697ea040a056560b7a33a78dd24cbb4ed620c1c
Marvel <marvel.delante@live.com>
2021-02-13 19:36:53 +0900

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ namespace ConsoleApp1
 {
     class Program
     {
-        // Create a list of students wiht property id,name,age,address
-        // and search student in the list using id
+        // Create a list of user which has a property of userid,username,fullname
+        // Search a user from the list using userid
         static void Main(string[] args)
         {
               
