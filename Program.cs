d872c7368e2941127a43286fd03fcdc95e8dba31
jee ar <jhayrbaidiango@gmail.com>
2021-02-16 21:43:37 +0800

    Finished

--- Program.cs
+++ Program.cs
@@ -13,16 +13,22 @@ namespace ConsoleApp1
         // and search student in the list using id
         static void Main(string[] args)
         {
-              Students();
-			  searching();
+
+              int ss = Convert.ToInt32(Console.ReadLine()); // Converting the Console.ReadLine() to 'int' since this was already been set to string 
+              Students(); // To add a data from Students to studentslist
+              searching(ss); // Passing the data came from ss to seaching
+              
               Console.Read();
         }
 
-        static void searching()
+        static void searching(int n) // 'int n' Retrieving the data from Main method 'int ss' and store it to n.
         {
             foreach(studentslist tss in stt)
 			{
-				Console.WriteLine(tss.id + " " + tss.name + " " + tss.age + " " + tss.address);
+                if (tss.id == n)
+                {
+                    Console.WriteLine(tss.id + " " + tss.name + " " + tss.age + " " + tss.address);
+                }	
 			}
         }
 
@@ -33,26 +39,27 @@ namespace ConsoleApp1
             st.name = "Ricardo";
             st.age = 12;
             st.address = "Taga bukid";
-            stt.Add(st);
+            stt.Add(st); //To store the data from stt to studentslist
 
-             studentslist st1 = new studentslist();
+            studentslist st1 = new studentslist();
             st1.id = 002;
             st1.name = "Bruno";
             st1.age = 16;
             st1.address = "Taga suba";
-            stt.Add(st1);
+            stt.Add(st1); //To store the data from stt to studentslist
 
-             studentslist st2 = new studentslist();
+            studentslist st2 = new studentslist();
             st2.id = 003;
             st2.name = "marlo";
             st2.age = 12;
             st2.address = "Taga baybay";
-            stt.Add(st);
+            stt.Add(st2); //To store the data from stt to studentslist
             
         }
 
         public class studentslist
         {
+            //To store all the data from Students()
             public int id 
             {
                 get;
